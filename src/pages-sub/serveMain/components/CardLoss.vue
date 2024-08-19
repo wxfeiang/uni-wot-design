f5c38022aca623a8b5f2ef249e5086a0ef643ea7
renchao <876666981@qq.com>
2024-08-19 14:35:53 +0800

    feat: 添加社保卡挂失功能

---
+++ src/pages-sub/serveMain/components/CardLoss.vue
@@ -0,0 +1,101 @@
+<script lang="ts" setup>
+import useCardLoss from '../hooks/useCardLoss'
+const { submitCard, model, rules, loading } = useCardLoss()
+
+const form = ref(null)
+
+const visible = ref<boolean>(false)
+
+function showKeyBoard() {
+  visible.value = true
+}
+</script>
+<template>
+  <view class="p-15px">
+    <view class="rounded-10px overflow-hidden bg-#fff">
+      <wd-form ref="form" :model="model">
+        <wd-cell-group border>
+          <wd-input
+            label="身份证号码:"
+            label-width="100px"
+            type="text"
+            v-model="model.zjhm"
+            placeholder="请输入身份证号码"
+            :rules="rules.zjhm"
+            prop="zjhm"
+            custom-input-class="custom-input-right"
+            disabled
+            @click="showKeyBoard"
+            :maxlength="18"
+            :mixlength="16"
+          />
+          <wd-input
+            label="姓名:"
+            label-width="100px"
+            type="text"
+            v-model="model.xm"
+            placeholder="请输入姓名"
+            :rules="rules.xm"
+            prop="xm"
+            custom-input-class="custom-input-right"
+          />
+          <wd-number-keyboard
+            v-model:visible="visible"
+            v-model="model.cardNumber"
+            :maxlength="18"
+            extra-key="X"
+            close-text="完成"
+          ></wd-number-keyboard>
+          <wd-input
+            label="社会保障卡号:"
+            label-width="100px"
+            type="text"
+            v-model="model.zhbzkh"
+            placeholder="请输入社会保障卡号"
+            :rules="rules.zhbzkh"
+            prop="zhbzkh"
+            custom-input-class="custom-input-right"
+          />
+
+          <wd-input
+            label="经办人:"
+            label-width="100px"
+            type="text"
+            v-model="model.jbr"
+            placeholder="请输入经办人"
+            :rules="rules.jbr"
+            prop="jbr"
+            custom-input-class="custom-input-right"
+          />
+        </wd-cell-group>
+      </wd-form>
+    </view>
+    <view class="mt-20px">
+      <wd-button
+        type="primary"
+        :round="false"
+        size="medium"
+        @click="submitCard(form)"
+        block
+        :loading="loading"
+      >
+        提 交
+      </wd-button>
+    </view>
+  </view>
+</template>
+<script lang="ts">
+export default {
+  options: {
+    styleIsolation: 'shared',
+  },
+}
+</script>
+<style lang="scss" scoped>
+:deep(.wd-input__error-message) {
+  @apply text-right!;
+}
+:deep(.custom-input-right) {
+  @apply text-right! color-#999999!;
+}
+</style>
