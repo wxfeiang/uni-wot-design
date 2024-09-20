ed776c669fce4e446cb752795cc6f7a637dff25c
刘少发 <12525753@qq.com>
2024-09-20 18:00:05 +0800

    bug修改

--- src/pages/shop/shopCar.vue
+++ src/pages/shop/shopCar.vue
@@ -29,10 +29,8 @@ onLoad(async () => {
 <template>
   <view class="w-full text-center position-relative font-600 line-height-40px">
     购物车数量
-    <view
-      style="left: 50%; background-color: #fb2549; transform: translate(-50%)"
-      class="position-absolute w-35px h-3px border-rd-3px pos-bottom-none"
-    ></view>
+    <view style="left: 50%; background-color: #fb2549; transform: translate(-50%)"
+      class="position-absolute w-35px h-3px border-rd-3px pos-bottom-none"></view>
   </view>
 
   <view class="list">
@@ -44,14 +42,10 @@ onLoad(async () => {
       </div>
 
       <view class="w-full mt-15px flex">
-        <wd-checkbox
-          v-model="value"
-          @change="handleChange"
-          style="align-self: center"
-        ></wd-checkbox>
+        <wd-checkbox v-model="value" @change="handleChange" style="align-self: center"></wd-checkbox>
         <wd-img :width="105" :height="105" :src="topbgBase64" custom-image="img" />
         <view class="ml-15px flex-1 flex flex-col justify-between">
-          <view class="w-210px name">overflow-hiddenhiddenhiddenhiddenhiddenhidden</view>
+          <view class="w-190px name">overflow-hiddenhiddenhiddenhiddenhiddenhidden</view>
           <view style="font-size: 14px; color: #757575">灰色</view>
           <view class="w-full">
             <wd-tag color="#FAA21E" bg-color="#FF6609" plain>7天无理由退货</wd-tag>
@@ -71,9 +65,7 @@ onLoad(async () => {
     </view>
   </view>
 
-  <view
-    class="bg-white pos-fixed h-80px pos-bottom-none flex w-full justify-between px-15px box-border items-center"
-  >
+  <view class="bg-white pos-fixed h-80px pos-bottom-none flex w-full justify-between px-15px box-border items-center">
     <wd-checkbox v-model="value" @change="handleChange">全选</wd-checkbox>
     <view class="flex items-center">
       <view class="flex items-center">
@@ -110,7 +102,7 @@ onLoad(async () => {
 
 .img {
   overflow: hidden;
-  border-radius: 5px;
+  border-radius: 5px !important;
 }
 
 .name {
