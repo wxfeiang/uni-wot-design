57e5dc96a028c715cb6fabff30fb27ad48a721b5
wxfeiang <wxfeiang@qq.com>
2024-09-20 16:33:00 +0800

    feat: 积分模块修改 ，app打包配置

--- src/pages-sub/marketManager/integral/utils/types.ts
+++ src/pages-sub/marketManager/integral/utils/types.ts
@@ -1,7 +1,31 @@
-interface signInRules {
-  id?: string
-  value: string | number
-  title: string | number
+interface stageList {
+  signInDay: string | number
+  signIntegral: string | number
+  highlight: string | number
+  isToday?: string | number
 }
 
-export type { signInRules }
+interface signInFoProps {
+  totalSignInDay: string | number
+  todayIntegral: string | number
+  totalIntegral: string | number
+  surplusIntegral: string | number
+  lxSignInDay: string | number
+  today: boolean
+  stageList: stageList[]
+}
+
+interface signInDetailRopos {
+  userId: string | number
+  kind: string
+  num: number
+  resource: string
+  opertionTime: string
+}
+
+interface signMess {
+  signIn: string
+  msg: string
+}
+
+export type { signInDetailRopos, signInFoProps, signMess }
