e31c229fac861ee2c9fa628e2eca2fd57afd7b1f
wxfeiang <wxfeiang@qq.com>
2024-09-22 11:51:21 +0800

    feat: 优惠券码展示

--- src/types/uni-pages.d.ts
+++ src/types/uni-pages.d.ts
@@ -32,6 +32,7 @@ interface NavigateToOptions {
        "/pages-sub/userManager/SocialSecurityCard/index" |
        "/pages-sub/userManager/SocialSecurityCard/indexback" |
        "/pages-sub/userManager/cardManager/index" |
+       "/pages-sub/marketManager/coupon/coupDeil" |
        "/pages-sub/marketManager/coupon/index" |
        "/pages-sub/marketManager/coupon/mycoupon" |
        "/pages-sub/marketManager/coupon/writeOffCoup" |
