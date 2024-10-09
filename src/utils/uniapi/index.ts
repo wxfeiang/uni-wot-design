70fb799839b09702e7c2aea3be7aaccc4837e0ee
812083837@qq.com <812083837@qq.com>
2024-10-09 09:26:37 +0800

    fix: 修改正式地址

--- src/utils/uniapi/index.ts
+++ src/utils/uniapi/index.ts
@@ -33,7 +33,8 @@ export const openEmbeddedMiniProgram = (path: string, extraData = {}, appId = VI
       appId,
       path,
       extraData, // 需要传递给目标小程序的数据
-      envVersion: 'develop', // trial
+      // envVersion: 'develop', // trial
+      envVersion: 'trial', // develop
       success(res) {
         // 打开成功
         resolve(res)
